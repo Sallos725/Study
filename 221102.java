aa8bc97b97bb301e0f742c90b4fccae764a011da
Sallos725 <94312485+Sallos725@users.noreply.github.com>
2022-11-02 12:37:06 +0900

    Create 221102.java

---
+++ 221102.java
@@ -0,0 +1,47 @@
+import java.util.Arrays;
+
+class Solution {
+    public int[] solution(int num, int total) {
+        int[] answer = new int[num];
+        for (int i = 0; i < num; i ++) {
+            if (num % 2 == 1) {
+                answer[num/2] = total/num;
+                int k = total/num;
+                int j = (num/2);
+                while (j > -1) {
+                    answer[j] = k--;
+                    j--;
+                }
+                int l = total/num;
+                int m = (num/2);
+                while (m < num) {
+                    answer[m] = l++;
+                    m++;
+                }
+            }
+            else {
+                answer[num/2 - 1] = total/num;
+                answer[num/2] = total/num;
+                int k = total/num;
+                int j = (num/2-1);
+                while (j > -1) {
+                    answer[j] = k--;
+                    j--;
+                }
+                int l = total/num;
+                int m = (num/2-1);
+                while (m < num) {
+                    answer[m] = l++;
+                    m++;
+                }
+            }
+
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(Arrays.toString(solution.solution(3, 12)));
+    }
+}
